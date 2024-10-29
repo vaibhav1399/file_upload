9087d5f6e6669e208370eaec7def61d7cc5bfb24
Amit Dwivedi <77052508+vaibhav1399@users.noreply.github.com>
2024-10-30 00:14:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import FileUpload from './components/FileUpload';
+
+const App = () => {
+  return (
+    <div>
+      <h1>File Upload</h1>
+      <FileUpload />
+    </div>
+  );
+};
+
+export default App;
